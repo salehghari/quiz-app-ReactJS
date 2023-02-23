8359a393deb269662d5dc220ccecc8b207375fd8
Saleh <salehghari8470@gmail.com>
2023-02-24 00:22:43 +0330

    add timer while quiz is in progress, for each question you'd have 10s

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -10,6 +10,7 @@ const decodeHTML = function (html) {
 }
 
 export default function Question() {
+
   const goodSentencesList = ["Nice!", "Nailed It!", "Wow!", "Good Job!", "Great!", "Well Done!"];
   const badSentencesList = ["Nope!", "0 IQ!", "Why? That Was Easy!", "You Stupid!", "Kidding Me?", "WTF?!", "Read Some Book For Sure!"];
   const [goodSentence, setGoodSentence] = useState("");
@@ -28,6 +29,32 @@ export default function Question() {
   const triedToStart = useSelector(state => state.quiz.triedToStart);
   const loading = useSelector(state => state.quiz.quiz.loading);
 
+  const [timeLeft, setTimeLeft] = useState(encodedQuestions.length * 10);
+
+  const radius = 20;
+  const circumference = 2 * Math.PI * radius;
+  const progress = (((encodedQuestions.length * 10) - timeLeft) / (encodedQuestions.length * 10)) * 100;
+
+  const progressOffset = circumference - (progress / 100) * circumference;
+
+  const progressBarStyle = {
+    strokeDasharray: circumference,
+    strokeDashoffset: progressOffset !== Infinity ? progressOffset : 0,
+    strokeWidth: 3,
+    stroke: "#565eff",
+    fill: "transparent"
+  };
+
+  useEffect(() => {
+    if (!timeLeft) return;
+
+    const timerId = setInterval(() => {
+      setTimeLeft(timeLeft - 1);
+    }, 1000);
+
+    return () => clearInterval(timerId);
+  }, [timeLeft]);
+
   useEffect(() => {
     const decodedQuestions = encodedQuestions.map(q => {
       return {
@@ -38,6 +65,7 @@ export default function Question() {
       }
     })
     setQuestions(decodedQuestions);
+    setTimeLeft(encodedQuestions.length * 10); // if "encodedQuestions" changed new "timeLeft" would be generated
   }, [encodedQuestions])
   
   const questionIndex = useSelector(state => state.quiz.index);
@@ -137,10 +165,39 @@ export default function Question() {
   if (!question) {
     return;
   }
+  if (!timeLeft) {
+    return (
+      <div className="mt-2">
+        Time's up! Your quiz has ended.
+      </div>
+    )
+  }
 
   return (
     <div className={`questions-section my-3 ${animation}`}>
-      <p>Question <span className="main-color">{questionIndex + 1}</span> Of <span className="main-color">{questions.length}</span></p>
+
+      <div className="d-flex justify-content-between align-items-center">
+        <p className="m-0">Question <span className="main-color">{questionIndex + 1}</span> Of <span className="main-color">{questions.length}</span></p>
+
+        <div style={{position: "relative", width: "50px", height: "50px"}} className="mb-2 d-flex justify-content-center align-items-center">
+          <svg viewBox="-5 -5 50 50" style={{transform: "rotate(-90deg)"}}>
+            <circle
+              r={radius}
+              cx="20"
+              cy="20"
+              style={{ stroke: "#d7dfff", strokeWidth: 3, fill: "transparent" }}
+            />
+            <circle
+              r={radius}
+              cx="20"
+              cy="20"
+              style={progressBarStyle}
+            />
+          </svg>
+
+          <div style={{position: "absolute"}}>{timeLeft}</div>
+        </div>
+      </div>
       <h4 className="fs-4 mb-3">{question.question}</h4>
       <ul className="answers p-0">
         {
